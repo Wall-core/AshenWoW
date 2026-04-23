78113d8a7a07514e537cafd0f1af88edd33b0afe
ratkosrb <ratkomladic2@abv.bg>
2026-04-23 03:30:56 +0300

    Fix case where nodiscard does not exist.

--- src/shared/nonstd/nodiscard.hpp
+++ src/shared/nonstd/nodiscard.hpp
@@ -4,8 +4,10 @@
 #if defined(__has_cpp_attribute)
   #if __has_cpp_attribute(nodiscard) >= 201907
     #define NODISCARD(msg) [[nodiscard(msg)]]
-  #else
+  #elif __has_cpp_attribute(nodiscard) > 0
     #define NODISCARD(msg) [[nodiscard]]
+  #else
+    #define NODISCARD(msg)
   #endif
 #else
   #define NODISCARD(msg)
